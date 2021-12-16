bf34a710bd216c6710632329b4420e3f8ac6334f
Valentin <vakevk@gmail.com>
2021-12-16 22:30:02 +0100

    Handle AMF version differences

--- source/amf.cpp
+++ source/amf.cpp
@@ -1,5 +1,23 @@
 #include "amf.h"
 
+#include "util.h"
+
+#include <AMF/core/Version.h>
+
+namespace {
+struct Version {
+  uint64_t original;
+  uint16_t major;
+  uint16_t minor;
+  uint16_t patch;
+  uint16_t build;
+
+  Version(uint64_t v)
+      : major{AMF_GET_MAJOR_VERSION(v)}, minor{AMF_GET_MINOR_VERSION(v)},
+        patch{AMF_GET_SUBMINOR_VERSION(v)}, build{AMF_GET_BUILD_VERSION(v)} {}
+};
+} // namespace
+
 Amf::Amf()
     : amf_dll{AMF_DLL_NAMEA},
       query_version{get_proc_address<AMFQueryVersion_Fn>(
@@ -17,7 +35,16 @@ amf_uint64 Amf::version() const {
 
 amf::AMFFactory &Amf::init() const {
   amf::AMFFactory *factory{nullptr};
-  if (init_(version(), &factory) != AMF_OK) {
+  const Version runtime{version()};
+  const Version compiletime{AMF_FULL_VERSION};
+  log(LOG_INFO, "AMF runtime version    : {}.{}.{} build {} raw {}",
+      runtime.major, runtime.minor, runtime.patch, runtime.build,
+      runtime.original);
+  log(LOG_INFO, "AMF compiletime version: {}.{}.{} build {} raw {}",
+      compiletime.major, compiletime.minor, compiletime.patch,
+      compiletime.build, compiletime.original);
+  // Use compiletime version for compatibility.
+  if (init_(compiletime.original, &factory) != AMF_OK) {
     throw std::runtime_error("init");
   }
   return *factory;
